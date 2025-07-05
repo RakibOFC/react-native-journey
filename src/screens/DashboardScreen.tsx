39ccb90df008f6b17ce1eea02bc728af935d90c1
Rakibul Islam <58856168+RakibOFC@users.noreply.github.com>
2025-07-05 15:21:49 +0600

    Add logout button in dashboard

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, ActivityIndicator, StatusBar, Text, StyleSheet, TouchableOpacity, } from 'react-native';
 
 const DashboardScreen = () => (
   <View style={styles.container}>
+    <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
     <Text style={styles.text}>Welcome to Dashboard!</Text>
+    
+    <TouchableOpacity
+      style={styles.logoutButton}
+      onPress={() => console.log('Button Pressed')}
+    >
+      <Text style={{ color: '#ffffff', fontWeight: 'bold' }}>Logout</Text>
+    </TouchableOpacity>
+
   </View>
 );
 
 export default DashboardScreen;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  text: { fontSize: 22, fontWeight: 'bold' },
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  logoutButton: {
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#ff3b30',
+    borderRadius: 5
+  }
 });
